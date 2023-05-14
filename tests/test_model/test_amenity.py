1c3c497a06f900776641b857fd4f3b354dd1edb3
gebretewodro73 <gebretewodros73@gmail.com>
2023-05-14 22:35:19 +0300

    tests/test_amenity.py

--- tests/test_model/test_amenity.py
+++ tests/test_model/test_amenity.py
@@ -1,14 +1,1 @@
-#!/usr/bin/python3
-"""Unit tests for Amenity class."""
-import unittest
-from models.amenity import Amenity
-
-
-class TestAmenity(unittest.TestCase):
-    """Define tests for Amenity class."""
-
-    def test_attribute_type(self):
-        """Test that the attributes of Amenity are strings."""
-        amenity = Amenity()
-        self.assertIsInstance(amenity.name, str)
-        self.assertEqual(amenity.name, "")
+../../amnencons/AirBnB_clone/tests/test_models/test_amenity.py
