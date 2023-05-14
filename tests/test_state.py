bf3444ce8099be41ac5677182326d47ac173b430
gebretewodro73 <gebretewodros73@gmail.com>
2023-05-14 23:20:40 +0300

    arrange files

--- tests/test_state.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from models.state import State
-
-
-class TestState(unittest.TestCase):
-    def test_attributes_initialization(self):
-        state = State()
-        self.assertEqual(state.name, "")
